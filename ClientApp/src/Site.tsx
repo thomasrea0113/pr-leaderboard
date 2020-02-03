fcce556a88fa07fd12b4a99afe466c6d0f5161dd
reat <thomas.rea@jtekt.com>
2020-02-03 22:09:45 +0000

    scaffold identity

--- ClientApp/src/Site.tsx
+++ ClientApp/src/Site.tsx
@@ -3,7 +3,10 @@
 
 import 'expose-loader?React!react';
 import 'expose-loader?ReactDOM!react-dom';
+
 import 'expose-loader?jQuery!jquery';
+import 'jquery-validation';
+import 'jquery-validation-unobtrusive';
 
 import 'bootstrap/dist/css/bootstrap.css';
 import './scss/site.scss';
