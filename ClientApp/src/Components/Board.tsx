789229ee9402cdb4f3c247c0e076e5475e1ddaa8
reat <thomas.rea@jtekt.com>
2020-03-01 18:25:20 +0000

    configure webpack dev server

--- ClientApp/src/Components/Board.tsx
+++ ClientApp/src/Components/Board.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 
-export const Board: React.FC<{
+export const BoardComponent: React.FC<{
     name: string;
 }> = ({ name }) => {
     return <div>board: {name}</div>;
 };
 
-export default Board;
+export default BoardComponent;
--- ClientApp/webpack.config.js
+++ ClientApp/webpack.config.js
@@ -4,130 +4,151 @@ const webpack = require('webpack');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const { StatsWriterPlugin } = require('webpack-stats-plugin');
 
-module.exports = {
-    mode: 'development',
-    entry: {
-        recommendations: ['./src/pages/Recommendations.tsx'],
-        home: ['./src/pages/Home.tsx'],
-        site: ['./src/Site.ts'],
-    },
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'js/[name].[hash].js',
+const outDir = path.resolve(__dirname, 'dist');
 
-        // exposing all components in a global Componentes variable
-        library: ['Components', '[name]'],
-        libraryTarget: 'var',
-        libraryExport: 'default',
-    },
-    devtool: false,
-    resolve: {
-        extensions: ['.js', '.jsx', '.json', '.ts', '.tsx'],
-    },
-    module: {
-        rules: [
-            {
-                test: /\.ts(x|)$/,
-                loader: ['babel-loader', 'ts-loader', 'eslint-loader'],
-                exclude: /node_modules/,
-            },
-            {
-                test: /\.js/,
-                exclude: /node_modules/,
-                enforce: 'pre',
-                loader: ['source-map-loader'],
-            },
-            {
-                test: /\.s[ac]ss$/,
-                exclude: /node_modules/,
-                use: [
-                    MiniCssExtractPlugin.loader,
-                    {
-                        loader: 'css-loader',
-                        options: {
-                            sourceMap: true,
+module.exports = env => {
+    const config = {
+        mode: 'development',
+        entry: {
+            recommendations: ['./src/pages/Recommendations.tsx'],
+            home: ['./src/pages/Home.tsx'],
+            site: ['./src/Site.ts'],
+        },
+        output: {
+            path: outDir,
+            filename: 'js/[name].[hash].js',
+
+            // exposing all components in a global Componentes variable
+            library: ['Components', '[name]'],
+            libraryTarget: 'var',
+            libraryExport: 'default',
+        },
+        devtool: false,
+        resolve: {
+            extensions: ['.js', '.jsx', '.json', '.ts', '.tsx'],
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.ts(x|)$/,
+                    loader: ['babel-loader', 'ts-loader', 'eslint-loader'],
+                    exclude: /node_modules/,
+                },
+                {
+                    test: /\.js/,
+                    exclude: /node_modules/,
+                    enforce: 'pre',
+                    loader: ['source-map-loader'],
+                },
+                {
+                    test: /\.s[ac]ss$/,
+                    exclude: /node_modules/,
+                    use: [
+                        MiniCssExtractPlugin.loader,
+                        {
+                            loader: 'css-loader',
+                            options: {
+                                sourceMap: true,
+                            },
                         },
-                    },
-                    'resolve-url-loader',
-                    {
-                        loader: 'sass-loader',
-                        options: {
-                            sourceMap: true,
+                        'resolve-url-loader',
+                        {
+                            loader: 'sass-loader',
+                            options: {
+                                sourceMap: true,
+                            },
                         },
-                    },
-                ],
-            },
-            {
-                test: /\.css$/,
-                include: /node_modules/,
-                use: [
-                    MiniCssExtractPlugin.loader, // will export css to a separate file
-                    // 'style-loader', // would inject the css via javascript
-                    'css-loader',
-                ],
-            },
-            {
-                test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
-                use: [
-                    {
-                        loader: 'file-loader',
-                        options: {
-                            name: '[name].[ext]',
-                            outputPath: '/fonts/',
+                    ],
+                },
+                {
+                    test: /\.css$/,
+                    include: /node_modules/,
+                    use: [
+                        MiniCssExtractPlugin.loader, // will export css to a separate file
+                        // 'style-loader', // would inject the css via javascript
+                        'css-loader',
+                    ],
+                },
+                {
+                    test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+                    use: [
+                        {
+                            loader: 'file-loader',
+                            options: {
+                                name: '[name].[ext]',
+                                outputPath: '/fonts/',
+                            },
                         },
-                    },
-                ],
-            },
-            {
-                test: require.resolve('react'),
-                use: [
-                    {
-                        loader: 'expose-loader',
-                        options: 'React',
-                    },
-                ],
-            },
-            {
-                test: require.resolve('react-dom'),
-                use: [
-                    {
-                        loader: 'expose-loader',
-                        options: 'ReactDOM',
-                    },
-                ],
-            },
-            {
-                test: require.resolve('jquery'),
-                use: [
-                    {
-                        loader: 'expose-loader',
-                        options: 'jQuery',
-                    },
-                    {
-                        loader: 'expose-loader',
-                        options: '$',
-                    },
-                ],
-            },
+                    ],
+                },
+                {
+                    test: require.resolve('react'),
+                    use: [
+                        {
+                            loader: 'expose-loader',
+                            options: 'React',
+                        },
+                    ],
+                },
+                {
+                    test: require.resolve('react-dom'),
+                    use: [
+                        {
+                            loader: 'expose-loader',
+                            options: 'ReactDOM',
+                        },
+                    ],
+                },
+                {
+                    test: require.resolve('jquery'),
+                    use: [
+                        {
+                            loader: 'expose-loader',
+                            options: 'jQuery',
+                        },
+                        {
+                            loader: 'expose-loader',
+                            options: '$',
+                        },
+                    ],
+                },
+            ],
+        },
+        plugins: [
+            // instead of using devtool, we will build the source pags automatically
+            new webpack.SourceMapDevToolPlugin({
+                filename: '[file].map',
+            }),
+            new MiniCssExtractPlugin({
+                filename: 'css/[name].[hash].css',
+            }),
+            // used by the Razor Page TagHelper. The helper reads the stats file to
+            // figured out the virtual path of the bundles
+            new StatsWriterPlugin({
+                stats: {
+                    all: false,
+                    assets: true,
+                    hash: true,
+                    publicPath: true,
+                },
+            }),
         ],
-    },
-    plugins: [
-        // instead of using devtool, we will build the source pags automatically
-        new webpack.SourceMapDevToolPlugin({
-            filename: '[file].map',
-        }),
-        new MiniCssExtractPlugin({
-            filename: 'css/[name].[hash].css',
-        }),
-        // used by the Razor Page TagHelper. The helper reads the stats file to
-        // figured out the virtual path of the bundles
-        new StatsWriterPlugin({
-            stats: {
-                all: false,
-                assets: true,
-                hash: true,
-                publicPath: true,
-            },
-        }),
-    ],
+    };
+
+    if (env && env.production) config.mode = 'production';
+
+    if (config.mode === 'development') {
+        config.devServer = {
+            contentBase: outDir,
+            compress: true,
+            host: '0.0.0.0',
+            port: 9000,
+            hot: true,
+            writeToDisk: true,
+        };
+
+        config.output.publicPath = 'http://localhost:9000';
+    }
+
+    return config;
 };
