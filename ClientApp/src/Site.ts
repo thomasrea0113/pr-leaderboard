65ff19e32e25e220f61aa34080729498ec634ef7
reat <thomas.rea@jtekt.com>
2020-03-12 19:51:03 +0000

    add fixed top nav

--- ClientApp/src/Site.ts
+++ ClientApp/src/Site.ts
@@ -13,3 +13,37 @@ import './scss/site.scss';
 $(document).ready($ => {
     $('[data-show=true]').modal('show');
 });
+
+// store the page's scroll-y position as a data attribute on the root html element
+(function trackScroll() {
+    const debounce = (fn: () => void) => {
+        // This holds the requestAnimationFrame reference, so we can cancel it if we wish
+        let frame: number;
+
+        // The debounce function returns a new function that can receive a variable number of arguments
+        return () => {
+            // If the frame variable has been defined, clear it now, and queue for next frame
+            if (frame) {
+                cancelAnimationFrame(frame);
+            }
+
+            // Queue our function call for the next frame
+            frame = requestAnimationFrame(() => {
+                // Call our function and pass any params we received
+                fn();
+            });
+        };
+    };
+
+    // Reads out the scroll position and stores it in the data attribute
+    // so we can use it in our stylesheets
+    const storeScroll = () => {
+        document.documentElement.dataset.scroll = window.scrollY.toString();
+    };
+
+    // Listen for new scroll events, here we debounce our `storeScroll` function
+    document.addEventListener('scroll', debounce(storeScroll));
+
+    // Update scroll position for first time
+    storeScroll();
+})();
