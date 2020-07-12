dfbe1b2cd90e02a5666d02cd747bf432161a3842
reat <thomas.rea@jtekt.com>
2020-07-12 19:03:48 +0000

    add other data operations

--- ClientApp/src/Components/tables/useDefaultTable.tsx
+++ ClientApp/src/Components/tables/useDefaultTable.tsx
@@ -4,6 +4,8 @@ import {
     TableInstance,
     useTable,
     SortingRule,
+    IdType,
+    Filters,
 } from 'react-table';
 import { useMemo } from 'react';
 import { mergeObjects } from '../../utilities/mergeObjects';
@@ -24,20 +26,22 @@ const parseSort = <T extends {}>(sortString: string): SortingRule<T> => {
     };
 };
 
+const parseFilters = <T extends {}>(filters: string[]): Filters<T> =>
+    filters.map(f => {
+        const [id, value] = f.split('=');
+        return { id, value };
+    });
+
 export const useDefaultTable = <T extends {}>(
     options: TableOptions<T>,
     ...plugins: PluginHook<T>[]
 ): TableInstance<T> => {
     const params = new URLSearchParams(window.location.search);
 
-    const orderBy = params.getAll('orderBy');
-    const orderDependencies = [orderBy.join('.')];
-
-    const sortBy: SortingRule<T>[] = useMemo(() => {
-        return orderBy.map(v => {
-            return parseSort(v);
-        });
-    }, orderDependencies);
+    const sortBy = params.getAll('orderBy').map(parseSort);
+    const groupBy = params.getAll('groupBy');
+    const filters = parseFilters(params.getAll('filterBy'));
+    const globalFilter = params.getAll('globalFilter').join(',');
 
     const defaultOptions: TableOptions<T> = {
         autoResetSortBy: true,
@@ -45,6 +49,9 @@ export const useDefaultTable = <T extends {}>(
         data: [],
         initialState: {
             sortBy,
+            groupBy,
+            filters,
+            globalFilter,
         },
     };
 
@@ -53,10 +60,9 @@ export const useDefaultTable = <T extends {}>(
     // options have to memoized (i think...), but we need to update the data when the
     // loading completes. So we memoize based on the search string params, and then
     // we allways append the latest data
-    const mergedOptions = useMemo(
-        () => mergeObjects(defaultOptions, options),
-        orderDependencies
-    );
+    const mergedOptions = useMemo(() => mergeObjects(defaultOptions, options), [
+        window.location.search,
+    ]);
 
     return useTable({ ...mergedOptions, data }, ...plugins);
 };
